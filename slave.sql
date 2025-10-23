2bf77ffae23952c9db12a6b5368494d3dedc1a19
bmw81 <bmw81@mail.ru>
2025-10-23 16:37:57 +0300

    2 tasks done

---
+++ slave.sql
@@ -0,0 +1,7 @@
+CHANGE REPLICATION SOURCE TO -- изменяем источник полученяи данных журнала в версиях
+до MySQL 8.0.23 ( CHANGE MASTER TO)
+SOURCE_HOST='mysql_master’, -- хост мастера
+SOURCE_USER='repl’, -- пользователь для репликации
+SOURCE_PASSWORD='slavepass’, -- пароль пользователя для репликации
+SOURCE_SSL=1; -- включаем ssl
+START REPLICA; -- запускаем реплику
